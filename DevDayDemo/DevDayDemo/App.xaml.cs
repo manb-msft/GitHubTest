e3731669095a8d68e6766257686120843d560228
Manigandan B <manigandan.balachandran@hotmail.com>
2018-03-24 16:33:56 +0530

    CI Test

--- DevDayDemo/DevDayDemo/App.xaml.cs
+++ DevDayDemo/DevDayDemo/App.xaml.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 using Xamarin.Forms;
 
 namespace DevDayDemo
@@ -18,10 +22,14 @@ namespace DevDayDemo
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
-		}
+            // Handle when your app starts
+            AppCenter.Start("ios=a99c1462-989e-4036-873f-4c6cd1648ec6;" + "uwp=0a2c3083-8525-4821-a6fc-147569f2ae67;" + "android=2270b668-7da3-452b-8b6d-2c990fa92f9b", typeof(Analytics), typeof(Crashes));
 
-		protected override void OnSleep ()
+
+
+        }
+
+        protected override void OnSleep ()
 		{
 			// Handle when your app sleeps
 		}

df699b1bce721c61c892349a6094fedf029feec8
Manigandan <35102509+manb-msft@users.noreply.github.com>
2018-04-06 15:24:53 +0530

    Update appcenter-post-clone.sh

--- DevDayDemo/DevDayDemo.Android/appcenter-post-clone.sh
+++ DevDayDemo/DevDayDemo.Android/appcenter-post-clone.sh
@@ -2,4 +2,4 @@
 
 # Download MobileVLCKit binary
 echo "Change directory to VLCKit.iOS, downloading from https://rocdevdata.blob.core.windows.net/config/app_build_files/MobileVLCKit"
-cd "$APPCENTER_SOURCE_DIRECTORY/VLC/VLCKit.iOS" && {curl -O "https://rocdevdata.blob.core.windows.net/config/app_build_files/MobileVLCKit"; echo "Download finished"}
+cd "$APPCENTER_SOURCE_DIRECTORY" && {curl -O "https://rocdevdata.blob.core.windows.net/config/app_build_files/MobileVLCKit"; echo "Download finished"}
